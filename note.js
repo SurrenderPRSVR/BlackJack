386aee0af097b4f7d5eb8cf4d6a87a6aaf25f19c
Heather <hflanders.prsvr@gmail.com>
2024-04-24 20:19:51 -0500

    InProgress

---
+++ note.js
@@ -0,0 +1,58 @@
+// // create two functions, add3Points(), and remove1Point()
+// // add/remove points to/from the myPoints variable
+// let myPoints = 3
+
+// function add3Points() {
+//     myPoints +=  3
+    
+// }
+
+// function remove1Point() {
+//     myPoints -=  1
+    
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Point()
+// remove1Point()
+// // Call the functions so that the line below logs out 10
+
+
+//When the user clicks the purchse button, render out
+//"Something went wrong, please try again"
+// that has the id="error"
+
+// let errorParagraph = document.getElementById("error")
+// console.log(errorParagraph)
+
+// function purchase () {
+//     console.log("button clicked")
+//     errorParagraph.textContent = "Something went wrong, please try again"
+// }
+
+// Create four functions: add(), subtract(), divide(), multiply()
+// Call the correct function when the user clicks on one of the buttons
+// Perform the given calculation useing num1 and num2
+// Render the result of the calculation in the paragraph with id="sum-el"
+// e.g if the user clicks on the "plus", you should render
+// "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
+
+// let num1 = 8
+// let num2 = 2
+// document.getElementId("num1-el").textContent = num1
+// document.getElementId("num2-el").textContent = num2
+
+// function add(){
+//     console.log("add clicked")
+// }
+// function subtract(){
+//     console.log("subtract clicked")
+// }
+// function divide(){
+//     console.log("multiply clicked")
+// }
+// function multiply(){
+//     console.log("divide clicked")
+// }
