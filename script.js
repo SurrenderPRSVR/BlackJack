475a410d2ed69eb9bf52e1144a1035e81eb32ee2
Heather <hflanders.prsvr@gmail.com>
2024-04-30 19:55:10 -0500

    InProgress

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
 
 
 let firstCard = 10
-let secondCard = 7
+let secondCard = 4
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-if(sum <= 20) {
-    message = "Do you want to draw a new card? 🤷‍♂️"
+
+function startGame() {
+sumEl.textContent = "Sum: " + sum
+cardsEl.textContent = "Cards:  " + firstCard + " " + secondCard
+
+if (sum <= 20) {
+    message = "Do you want to draw a new card?"
+    
 } else if (sum === 21) {
-    message = "Woohoo! You've got BlackJack! 😁"
+    message = "You've got BlackJack!"
     hasBlackJack = true
 } else  {
-    message = "You're out of the game!😒"
+    message = "You're out of the game!"
     isAlive = false
+}
+messageEl.textcontent = message
+
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!")
 }
 
 
-console.log(message)
+
